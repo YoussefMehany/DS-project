7134d8f0b44858c4aaa08435f8d421d842ad51c1
Muhammad Amr <midoamr20@gmail.com>
2023-04-08 17:21:36 +0200

    Process Class

---
+++ DSProject/DSProject/Process.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Enums.h"
+class Process
+{
+private:
+	static int counter;
+	int PID;
+	int ArrivalTime;
+	int ResponseTime;
+	int CPUTime;
+	int TerminationTime;
+	int TurnAroundDuration;
+	int WaitingTime;
+	int IORequestTime;
+	int IODuration;
+public:
+	Process(int ArrivalTime, int CPUTime , int IORequestTime , int IODuration);
+	int GetPID()const;
+	int GetArrivalTime()const;
+	int GetCPUTime()const;
+	int GetTerminationTime()const;
+	int GetResponseTime()const;
+	int GetTurnAroundDuration()const;
+	int GetWaitingTime()const;
+	int GetTurnAroundDuration()const;
+	int GetWaitingTime()const;
+	int GetIORequestTime()const;
+	int GetIODuration()const;
+	void SetTerminationTime(int TerminationTime);
+	void SetResponseTime(int FirstTime);
+	void SetTurnAroundDuration();
+	void SetWaitingTime();
+};
+
