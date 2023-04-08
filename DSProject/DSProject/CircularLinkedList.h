0f2b21abdae9d35741555fed44c27db239eb6b9c
YoussefMehany <117949033+YoussefMehany@users.noreply.github.com>
2023-04-09 00:05:25 +0200

    Data Structures

---
+++ DSProject/DSProject/CircularLinkedList.h
@@ -0,0 +1,102 @@
+#pragma once
+#include"Node.h"
+template <class T>
+class CirLinkedList
+{
+	Node<T>* Head;
+	int GetLen(Node<T>* head) const;
+public:
+	CirLinkedList() { Head = NULL; }
+	bool Empty()const { return !Head; }
+	int Size()const { return GetLen(Head); }
+	Node<T>* head()const { return Head; }
+	Node<T>*& hhead() { return Head; }
+	void InsertBeg(T Val)
+	{
+		Node<T>* temp = new Node<T>(Val), * tail = Head;
+		while (tail && tail->getNext() != Head)
+			tail = tail->getNext();
+		temp->setNext(Head);
+		if (!Head)
+			temp->setNext(temp);
+		else
+			tail->setNext(temp);
+		Head = temp;
+	}
+	void InsertEnd(T Val)
+	{
+
+		Node<T>* temp = new Node<T>(Val);
+		if (!Head) {
+			Head = temp;
+			Head->setNext(Head);
+			return;
+		}
+		Node<T>* tail = Head;
+		while (tail->getNext() != Head)
+			tail = tail->getNext();
+		tail->setNext(temp);
+		temp->setNext(Head);
+	}
+	bool RemoveBeg()
+	{
+		if (!Head)
+			return false;
+		Node<T>* tail = Head, * temp2 = Head;
+		while (tail->getNext() != Head)
+			tail = tail->getNext();
+		if (Head == Head->getNext()) {
+			delete Head;
+			Head = nullptr;
+		}
+		else {
+			Head = Head->getNext();
+			tail->setNext(Head);
+			delete temp2;
+		}
+		return true;
+	}
+	bool Remove(const T& Element)
+	{
+		if (!Head)
+			return false;
+		if (Element == Head->getItem())
+			RemoveBeg();
+		else
+		{
+			Node<T>* temp = Head, * bye = nullptr;
+			while (temp->getNext()->getItem() != Element) {
+				if (temp->getNext() == Head)
+					return false;
+				temp = temp->getNext();
+			}
+			bye = temp->getNext();
+			temp->setNext(bye->getNext());
+			delete bye;
+		}
+		return true;
+	}
+	void Print()const
+	{
+		Node<T>* temp = Head;
+		if (!temp)
+			return;
+		cout << "[ ";
+		while (temp->getNext() != Head)
+		{
+			cout << temp->getItem() << " ";
+			temp = temp->getNext();
+		}
+		cout << temp->getItem() << " " << "]" << endl;
+
+	}
+};
+template<class T>
+int CirLinkedList<T>::GetLen(Node<T>* head) const
+{
+	if (!head)
+		return 0;
+	if (head->getNext() == Head)
+		return 1;
+	return 1 + GetLen(head->getNext());
+}
